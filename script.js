00deeb9fcc0dc3d004d678643928b06670e34e41
Sandra.B <105020067+Sandra@users.noreply.github.com>
2024-01-25 12:46:07 +0100

    updated javascript and html

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const gridListButtons = document.querySelectorAll('.grid-list-button');
+document.addEventListener('DOMContentLoaded', function () {
+    const gridListButton = document.querySelector('.grid-list-button');
 
-gridListButtons.forEach(button => {
-    button.addEventListener('click', gridListToggle);
+    gridListButton.addEventListener('click', function () {
+        gridListToggle(gridListButton);
+    });
 });
 
-function gridListToggle() {
-    let originalButton = this;
-
-    originalButton.classList.add('animation');
-    originalButton.classList.toggle('list');
+function gridListToggle(button) {
+    button.classList.add('animation');
+    button.classList.toggle('list');
 
     // Update the icon based on the current view
-    const icon = originalButton.querySelector('.icon');
-    if (originalButton.classList.contains('list')) {
+    const icon = button.querySelector('.icon');
+    if (button.classList.contains('list')) {
         // Update for list view
-        icon.innerHTML = '<div class="lines"><i></i><i></i><i></i><i></i></div>';
+        icon.innerHTML = '<span class="lines"><i></i><i></i><i></i><i></i></span>';
     } else {
         // Update for grid view
-        icon.innerHTML = '<div class="dots"><i></i><i></i><i></i><i></i></div>';
+        icon.innerHTML = '<span class="dots"><i></i><i></i><i></i><i></i></span>';
     }
 
     // Remove animation class after animation completes
     setTimeout(() => {
-        originalButton.classList.remove('animation');
+        button.classList.remove('animation');
     }, 400);
 }

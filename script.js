47cb893e56b7ec67a3aaf3e024f0460aad848fad
Sandra.B <105020067+Sandra@users.noreply.github.com>
2024-01-25 12:27:39 +0100

    grid/list button

---
+++ script.js
@@ -0,0 +1,21 @@
+const gridListButtons = document.querySelectorAll('.grid-list-button');
+
+gridListButtons.forEach(button => {
+    button.addEventListener('click', gridListToggle);
+});
+
+function gridListToggle() {
+    let originalButton = this;
+    
+    originalButton.classList.add('animation');
+    originalButton.classList.toggle('list');
+
+    // Clone the original button
+    let clonedButton = originalButton.cloneNode(true);
+
+    // Replace the original button with the cloned one
+    originalButton.parentNode.replaceChild(clonedButton, originalButton);
+
+    // Add a click event listener to the cloned button
+    clonedButton.addEventListener('click', gridListToggle);
+}
